b5eec74b3a2dec53cdfae3902c7e5a19be3d1343
Jose <jozylopez27@gmail.com>
2020-06-03 15:56:59 -0500

    removed did mount func from card component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {Route, Switch} from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage'
 import Header from './Components/Header/Header'
 import './App.css';
+import API from './api';
+import UserContext from './Context/UserContext';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       games: []
-    }
+  static contextType = UserContext
+
+  async componentDidMount() {
+    this.context.clearError()
+    const res = await API.get('/app-load')
+    .catch(e => this.context.setError(e.message))
+    this.context.setGames(res.data.gameData)
+    this.context.setGenres(res.data.genreData)
   }
 
   render() {
