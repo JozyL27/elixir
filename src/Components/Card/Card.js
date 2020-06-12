1116d78b0516ba51b7205b1fabc99defc08d702c
Gage Eide <gageeide@gmail.com>
2020-06-11 21:51:17 -0500

    Got transparent background working

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,36 +3,61 @@ import UserContext from '../../Context/UserContext';
 import './Card.css';
 export default class Card extends Component {
     static contextType = UserContext
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
 
-    cardClickHandler = (e) => {
-        //* Get ID from value attribute of clicked element
-        const gameId = parseInt(e.currentTarget.attributes.id.value);
-        //* Get parent card from
-        console.log(gameId);
-	}
+    cardClickHandler = () => {
+        this.setState({
+            active: true
+        });
+    };
+    
+    activeClickHandler = () => {
+        this.setState({
+            active: false
+        });
+    };
 	
 	render() {
-		const game = this.props.game;
+        console.log(this.state.active);
+        const game = this.props.game;
 		return (
-			<li key={game.id} id={game.id} className='card'
-                    onClick={this.cardClickHandler}
-                    style={{
-                        background: game.cover ? 
-                        `url(${game.cover.url.replace('thumb', 'cover_big')})` : 'black',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundSize: 'auto 100%',
-                        backgroundPosition: 'center',
-                    }}  
+            this.state.active ?
+            //* Active Card
+            <div className='background' onClick={this.activeClickHandler}>
+                <li key={game.id} id={game.id} 
+                    className='active-card card'
+                    onClick={null}
                 >
+                    {game.cover ? 
+                    <img src={game.cover.url.replace('thumb', 'cover_big')} className='cover' alt={`${game.name} Cover Art`}/>
+                    : <p className='unavailable'>
+                        no cover art available</p>}
                     <div className='cardOverlay'>
                         <h3 className='gameTitle'>{game.name}</h3>
                         <p className='gameSummary'>{game.summary}</p>
                     </div>
-                    {game.cover ? 
-                    null 
-                    : <p className='unavailable'>
-                        no cover art available</p>}
                 </li>
-		)
-	}
+            </div>
+            :
+            //* Inactive Card
+            <li key={game.id} id={game.id} 
+                className='card'
+                onClick={this.cardClickHandler}
+            >
+                {game.cover ? 
+                <img src={game.cover.url.replace('thumb', 'cover_big')} className='cover' alt={`${game.name} Cover Art`}/>
+                : <p className='unavailable'>
+                    no cover art available</p>}
+                <div className='cardOverlay'>
+                    <h3 className='gameTitle'>{game.name}</h3>
+                    <p className='gameSummary'>{game.summary}</p>
+                </div>
+            </li>
+		);
+	};
 }
